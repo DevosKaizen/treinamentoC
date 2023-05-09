93ee048a29e9e85f382c455d61a85f3428345bb0
AndrewDevos <andrew.cottadevos@gmail.com>
2023-05-09 15:13:59 -0300

    funcoes total carai

--- listadeexercicios1DIANA.c
+++ listadeexercicios1DIANA.c
@@ -4,23 +4,53 @@
 
 //1. Solicite ao usuário dois inteiros e exiba a soma, o produto e a divisão desses dois números.
 //vamos fazer com do while
+int exercicio1(int a, int b){
 
-int soma(int a, int b){
-    int resultado;
-    resultado = a + b;
-    return resultado;
-}
-int produto(int a, int b){
-    int resultado;
-    resultado = a * b;
-    return resultado;
-}
-int divisao(int a, int b){
-    int resultado;
-    resultado = a / b;
-    return resultado;
-}
+    
+    int resultadosoma ;
+    int resultadoproduto ; 
+    int resultadodivisao ;
 
+
+    printf("\n vamos entrar no execicio");
+    
+    printf("\n 1. Solicite ao usuario dois inteiros e exiba a soma, o produto e a divisao desses dois numeros.\n");
+    
+    printf("\n Digite o primeiro numero inteiro: ");
+    scanf("%d", &a);
+    
+    printf("\n Digite o segundo numero inteiro: ");
+    scanf("%d", &b);
+    
+    printf("\n Os inteiros foram: %d e %d\n\n", a, b);
+    
+   
+
+    int soma(int a, int b){
+        int resultado;
+        resultado = a + b;
+        printf("\n A soma dos inteiros e: %d", resultadosoma);
+        return resultado;
+    }
+    int produto(int a, int b){
+        int resultado;
+        resultado = a * b;
+        printf("\n O produto dos inteiros e: %d", resultadoproduto);
+        return resultado;
+    }
+    int divisao(int a, int b){
+        int resultado;
+        resultado = a / b;
+        printf("\n A divisao dos inteiros e: %d", resultadodivisao);
+        return resultado;
+    
+    
+    }
+
+    resultadosoma = soma(a, b);
+    resultadoproduto = produto(a, b);
+    resultadodivisao = divisao(a, b);
+}
 /*double area(double c){
     double c;
     
@@ -38,35 +68,15 @@ int main(){
 
     int a ;
     int b ;
-    double c;
-    double d;
-
     int resultadosoma ;
     int resultadoproduto ; 
     int resultadodivisao ;
 
-    printf("\n Vai tomar no cu caralho\n");
-    printf("\n vamos entrar no execicio");
-    
-    printf("\n 1. Solicite ao usuario dois inteiros e exiba a soma, o produto e a divisao desses dois numeros.\n");
-    
-    printf("\n Digite o primeiro numero inteiro: ");
-    scanf("%d", &a);
-    
-    printf("\n Digite o segundo numero inteiro: ");
-    scanf("%d", &b);
-    
-    printf("\n Os inteiros foram: %d e %d\n\n", a, b);
-    
-    resultadosoma = soma(a, b);
-    resultadoproduto = produto(a, b);
-    resultadodivisao = divisao(a, b);
+    double c;
+    double d;
 
-    
-    
-    printf("\n A soma dos inteiros e: %d", resultadosoma);
-    printf("\n O produto dos inteiros e: %d", resultadoproduto);
-    printf("\n A divisao dos inteiros e: %d", resultadodivisao);
+    printf("\n Vai tomar no cu caralho\n");
+    exercicio1(a, b);
 
     printf("\n\n AGORA VAMOS CALCULAR A AREA");
 
@@ -94,9 +104,9 @@ int main(){
 
     printf("3. Calcule e apresente o valor do volume de uma lata de oleo, utilizando a formula: VOLUME = 3.14159 * RAIO2 * ALTURA");
     printf("\n Qual o raio? \n Digite aqui "); 
-    scanf("%lf\n", &raio);
+    scanf("%lf", &raio);
     printf("\n Qual a altura? \n Digite aqui "); 
-    scanf("%lf\n", &altura);
+    scanf("%lf", &altura);
     
     //função real do exercicio
     raio = raio * raio;
